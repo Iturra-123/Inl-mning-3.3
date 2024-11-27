dd4313b9d89c14131714edb0d68d804e3fae645a
leit07001 <leit07001@E-PW05C37S>
2024-11-27 12:00:57 +0100

    Add project files.

---
+++ Inlämning 3.3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Numerics;
+namespace inlämning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int timpris = 80;
+
+            const int maxPrisPerDag = 950;
+
+            Console.WriteLine("Hur många timmar vill du låna en bil för?");
+            int timmar =  int.Parse(Console.ReadLine());
+
+            int totalkostnad = timmar * timpris;
+
+            if (totalkostnad > maxPrisPerDag)
+            {
+                totalkostnad = maxPrisPerDag;
+                Console.WriteLine("Kostnaden för {0} timmar blir: {1} kronor");
+            }
+        }
+    }
+}
